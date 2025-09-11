753ab9c1d8760e9b79922cecc14a540a789c49d9
Daniel Viola <daniel.viola001@mymdc.net>
2025-09-11 12:28:00 -0400

    first commit

---
+++ Sept 11th, 2025/mips1.asm
@@ -0,0 +1,19 @@
+.data                   
+hello: .asciiz "Hello, World\n"  
+
+.text                   
+.globl main            
+
+main:
+
+    la $a0, hello       
+
+
+    li $v0, 4          
+
+
+    syscall
+
+
+    li $v0, 10         
+    syscall
