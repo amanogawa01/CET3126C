1af5edc7adbfc069421c8fda4bae881b9b07851f
Daniel Viola <daniel.viola001@mymdc.net>
2025-10-05 22:34:28 -0400

    forgor to annotate

--- October 5th, 2025/mips3.asm
+++ October 5th, 2025/mips3.asm
@@ -4,7 +4,7 @@
 
 main:
     li   $a0, 5         
-    jal  factorial     
+    jal  factorial     # Jump to factorial
     move $a0, $v0
     li   $v0, 1         
     syscall
@@ -17,9 +17,9 @@ main:
 
 
 factorial:
-    addi $sp, $sp, -8
-    sw   $ra, 4($sp)
-    sw   $a0, 0($sp)
+    addi $sp, $sp, -8  # allocate 8 bytes for the stack
+    sw   $ra, 4($sp)   # [SP+4] = return address
+    sw   $a0, 0($sp)   # [SP+0] = argument n
 
     li   $t0, 1
     ble  $a0, $t0, factorial_base   
@@ -33,6 +33,13 @@ factorial:
     lw   $ra, 4($sp)
     addi $sp, $sp, 8
     jr   $ra
+
+# Example: Factorial(5) call chain
+# factorial(5) top -> n=5, ra=main
+# factorial(4) top -> [n=4, ra=fact(5)]
+# factorial(3) top -> [n=3, ra=fact(4)]
+# factorial(2) top -> [n=2, ra=fact(3)]
+# factorial(1) top-> [n=1, ra=fact(2)]
 
 factorial_base:
     li   $v0, 1
